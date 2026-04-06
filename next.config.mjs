723ae930839fa0c5f15cdc2669cce2101fa1d485
Thanh Vo <thanhvoitbacore@gmail.com>
2026-04-07 00:05:43 +0700

    update 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Ensure that we can use the same aliases as in Vite
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': './src',
-    };
-    return config;
-  },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
